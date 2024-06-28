278b73762bc839e920a555de33689c62fc5aae3d
Martijn Schuman <martijnschuman@hotmail.com>
2024-06-28 02:02:18 +0200

    Opdrachten v1 & Action

---
+++ uitwerking/opdracht2/backend/backend/Migrations/20240625202117_ischecked.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations {
+    /// <inheritdoc />
+    public partial class ischecked : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsChecked",
+                table: "Items",
+                type: "boolean",
+                nullable: true,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropColumn(
+                name: "IsChecked",
+                table: "Items");
+        }
+    }
+}
